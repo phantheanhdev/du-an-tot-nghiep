c6b8ed349ae546ef66fb0647b18ced29a7563be2
phantheanhdev <phananh.main@gmail.com>
2023-10-04 11:15:04 +0700

    edit front user by theanh 4/10

---
+++ resources/views/user/layout/header.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<!-- Mirrored from askbootstrap.com/preview/eatsome/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 19 Sep 2023 03:05:09 GMT -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/logo.png" type="image/png">
+    <title>Eatsome - Restaurant & Food Delivery Mobile Template</title>
+
+    <link rel="stylesheet" href="{{ asset('user/askbootstrap/preview/vender/bootstrap/css/bootstrap.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('user/askbootstrap/preview/vender/icons/icofont.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('user/askbootstrap/preview/vender/slick/slick/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/askbootstrap/preview/vender/slick/slick/slick-theme.css') }}">
+    <link href="{{ asset('user/askbootstrap/preview/vender/fontawesome/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('user/askbootstrap/preview/vender/sidebar/demo.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('user/askbootstrap/preview/css/style.css') }}">
+</head>
+
+<body class="bg-light">
