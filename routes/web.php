64a2357a1de498c8c91b58906079435a074a76e9
phantheanhdev <phananh.main@gmail.com>
2023-10-28 23:24:04 +0700

    edit file web

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Events\HelloPusherEvent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QrController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\OrderController;
@@ -42,14 +44,13 @@ Route::get('qr-builder', [QrController::class, 'qr_builder'])->name('qr-builder'
 // table
 Route::resource('table', TableController::class);
 
-
-    //staff
-    Route::prefix('staff')->group(function () {
-        Route::get('/', [EmployeeController::class, 'index'])->name('employee.index');
-        Route::match(['get','post'], 'create', [EmployeeController::class, 'create'])->name('employee.create');
-        Route::match(['get', 'post'], 'edit/{id}', [EmployeeController::class, 'edit'])->name('employee.edit');
-        Route::get('delete/{id}', [EmployeeController::class, 'delete'])->name('employee.delete');
-    });
+//staff
+Route::prefix('staff')->group(function () {
+    Route::get('/', [EmployeeController::class, 'index'])->name('employee.index');
+    Route::match(['get', 'post'], 'create', [EmployeeController::class, 'create'])->name('employee.create');
+    Route::match(['get', 'post'], 'edit/{id}', [EmployeeController::class, 'edit'])->name('employee.edit');
+    Route::get('delete/{id}', [EmployeeController::class, 'delete'])->name('employee.delete');
+});
 
 
 //category
@@ -65,7 +66,7 @@ Route::prefix('category')->group(function () {
 // order
 Route::get('list-order', [OrderController::class, 'index']);
 
-Route::get('order/menu',[MenuController::class,'index']);
+Route::get('order/menu', [MenuController::class, 'index']);
 
 
 Route::resource('bill', BillController::class);
@@ -77,11 +78,11 @@ Route::get('/test', function () {
     return view('showNotification');
 });
 
-Route::get('getPusher', function (){
-   return view('form_pusher');
+Route::get('getPusher', function () {
+    return view('form_pusher');
 });
 
-Route::get('/pusher', function(Illuminate\Http\Request $request) {
+Route::get('/pusher', function (Illuminate\Http\Request $request) {
     event(new HelloPusherEvent($request));
     return redirect('getPusher');
 });
