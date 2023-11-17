4c169ff254619b34d51f2e282985dbef3f59f9c4
chienlehong <chienlhph19644@fpt.edu.vn>
2023-11-17 23:03:11 +0700

    phan trang

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -14,16 +14,16 @@ class AdminDashboardController extends Controller
     public function index()
     {
         //Thống kê doanh thu theo ngày - tháng - năm
-        $todaysOrder = Order::whereDate('created_at', Carbon::today())->where('status', 2)->count();
-        $totalPendingOrders = Order::where('status', 0)->count();
+        $todaysOrder = Order::whereDate('created_at', Carbon::today())->where('status', 5)->count();
+        $totalPendingOrders = Order::where('status', 5)->count();
 
-        $todaysEarnings = Order::where('status', 2)
+        $todaysEarnings = Order::where('status', 5)
             ->whereDate('order_day', Carbon::today())
             ->sum('total_price');
-        $monthEarnings = Order::where('status', 2)
+        $monthEarnings = Order::where('status', 5)
             ->whereMonth('created_at', Carbon::now()->month)
             ->sum('total_price');
-        $yearEarnings = Order::where('status', 2)
+        $yearEarnings = Order::where('status', 5)
             ->whereYear('created_at', Carbon::now()->year)
             ->sum('total_price');
 
@@ -46,11 +46,11 @@ class AdminDashboardController extends Controller
         $totalProducts = Product::count();
 
 
-        
+
 
         $statisticsMonth = DB::table('orders')
             ->select(DB::raw('MONTH(created_at) as month, COUNT(*) as total_orders, SUM(total_price) as total_amount'))
-            ->where('created_at', '>=', now()->subMonths(6))->where('status', 2) // Lấy thông tin từ 6 tháng trước đến hiện tại
+            ->where('created_at', '>=', now()->subMonths(6))->where('status', 5) // Lấy thông tin từ 6 tháng trước đến hiện tại
             ->groupBy('month')
             ->get();
 
@@ -60,7 +60,7 @@ class AdminDashboardController extends Controller
 
         $statisticsYear = DB::table('orders')
             ->select(DB::raw('YEAR(created_at) as year, COUNT(*) as total_orders, SUM(total_price) as total_amount'))
-            ->where('created_at', '>=', now()->subYears(3))->where('status', 2) // Lấy thông tin từ 3 năm trước đến hiện tại
+            ->where('created_at', '>=', now()->subYears(3))->where('status', 5) // Lấy thông tin từ 3 năm trước đến hiện tại
             ->groupBy('year')
             ->get();
 
